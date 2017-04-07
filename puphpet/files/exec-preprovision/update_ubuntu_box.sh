0670489e3830ac936d8db64c18e608809c8108df
Metodi Deribanov <imaginois@gmail.com>
2017-04-07 16:00:14 +0300

    move the update-ubuntu script

--- puphpet/files/exec-preprovision/update_ubuntu_box.sh
+++ puphpet/files/exec-preprovision/update_ubuntu_box.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-rm -f /etc/apt/sources.list.d/repo.mysql.com-apt.list
-#apt-get update
-#apt-get upgrade -y
+sudo rm -f /etc/apt/sources.list.d/repo.mysql.com-apt.list
+apt-get update
+apt-get upgrade -y
