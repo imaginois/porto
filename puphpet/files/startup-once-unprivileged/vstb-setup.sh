adb71134dcb234ea3e46ce8575c57dfe243342da
Metodi Deribanov <imaginois@gmail.com>
2017-04-07 16:01:51 +0300

    add staging.vstb.local to apache vhosts

--- puphpet/files/startup-once-unprivileged/vstb-setup.sh
+++ puphpet/files/startup-once-unprivileged/vstb-setup.sh
@@ -3,3 +3,8 @@ cd /var/www/vstb/ML/src
 sudo npm install
 grunt build --skin=revgo
 grunt css --skin=revgo
+
+cd /var/www/vstb/staging/src
+sudo npm install
+grunt build --skin=revgo
+grunt css --skin=revgo
--- puphpet/files/startup-once/setup_motd.sh
+++ puphpet/files/startup-once/setup_motd.sh
@@ -8,5 +8,5 @@ apt-get install lsb-release
 apt-get install figlet
 cp /vagrant/puphpet/files/motd/* /etc/update-motd.d/
 chmod +x /etc/update-motd.d/*
-rm /etc/motd
+rm -Rf /etc/motd
 ln -s /var/run/motd /etc/motd
