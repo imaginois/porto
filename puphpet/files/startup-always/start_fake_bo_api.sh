be571a0ccd9a986451cb28c79d86ad445d7655d8
Metodi Deribanov <imaginois@gmail.com>
2017-04-21 05:18:14 +0300

    fake BO working for vstb and stb

--- puphpet/files/startup-always/start_fake_bo_api.sh
+++ puphpet/files/startup-always/start_fake_bo_api.sh
@@ -2,4 +2,4 @@
 	# json-server /var/www/radio/api/stb.json --port 4000
 	# json-server /var/www/radio/api/vstb.json --port 4001
 docker run -d -p 5000:80 -v /var/www/radio/api/vstb.json:/data/db.json clue/json-server
-docker run -d -p 5001:80 -v /var/www/radio/api/stb.json:/data/db.json clue/json-server
+docker run -d -p 5001:80  -v /var/www/radio/api/stb.json:/data/db.json clue/json-server
