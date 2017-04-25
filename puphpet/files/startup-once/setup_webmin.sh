e00c30c372c83f1dc5948d8f2308af0baf883fef
Metodi Deribanov <imaginois@gmail.com>
2017-04-25 22:20:18 +0300

    add webmin setup script

---
+++ puphpet/files/startup-once/setup_webmin.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cd /etc/apt/sources.list.d/
+touch webmin.list
+echo "deb http://download.webmin.com/download/repository sarge contrib" >> webmin.list
+echo "deb http://webmin.mirror.somersettechsolutions.co.uk/repository sarge contrib" >> webmin.list
+cd /tmp
+wget http://www.webmin.com/jcameron-key.asc
+apt-key add jcameron-key.asc
+apt-get update
+apt-get install webmin -y
+sudo ufw allow 10000
