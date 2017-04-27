611a4b390f5d38a171a5ce905ba157db8e8c8a19
Metodi Deribanov <imaginois@gmail.com>
2017-04-27 19:35:35 +0300

    try to install libapache-mod-php

--- puphpet/files/startup-once-unprivileged/ladybug-setup-script.sh
+++ puphpet/files/startup-once-unprivileged/ladybug-setup-script.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
-if [[ ! -f '/usr/local/bin/composer' ]]; then
+if [ ! -f '/usr/local/bin/composer' ]; then
     echo " @@@@@ Composer is not installed on this machine. Type sudo apt-get install composer to install."
-    exit 1
+    exit 2
 fi
 
 cd /var/www/ladybug
 composer dump-autoload
 composer install --no-scripts
 
-sudo apt-get install nodejs
+sudo apt-get autoremove -y
+sudo apt-get install nodejs -y
+# sudo apt-get install libapache2-mod-php
 sudo ln -s /usr/bin/nodejs /usr/bin/node
+sudo chown -R $USER:$(id -gn $USER) /home/vagrant/.config
 sudo npm i -g npm@^4
+sudo npm i -g gulp
 sudo npm i
 gulp
 
