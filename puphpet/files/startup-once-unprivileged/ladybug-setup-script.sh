87b82c64d876b7d87a83caf6ab040eaa5ad61c6d
Metodi Deribanov <imaginois@gmail.com>
2017-04-29 21:17:56 +0300

    ladybug setup script changes

--- puphpet/files/startup-once-unprivileged/ladybug-setup-script.sh
+++ puphpet/files/startup-once-unprivileged/ladybug-setup-script.sh
@@ -4,27 +4,37 @@ if [ ! -f '/usr/local/bin/composer' ]; then
     exit 2
 fi
 
+echo "Downloading ladybug repo..."
+cd /var/www/
+ssh-keyscan bitbucket.org >> ~/.ssh/known_hosts
+git clone git@bitbucket.org:minervanetworks/ladybug.git
+
 cd /var/www/ladybug
 composer dump-autoload
 composer install --no-scripts
 
+#php artisan migrate
+#php artisan db:seed
+composer dump-autoload --optimize
+composer update
+php artisan key:generate
+
 sudo apt-get autoremove -y
-sudo apt-get install nodejs -y
-sudo apt-get install libapache2-mod-php
+sudo apt-get install libapache2-mod-php php-mbstring php-mysql -y
 sudo a2dismod worker
+echo "Restarting apache service:"
 sudo service apache2 restart
 sudo ln -s /usr/bin/nodejs /usr/bin/node
 sudo chown -R $USER:$(id -gn $USER) /home/vagrant/.config
-sudo npm i -g npm@^4
+# sudo npm i -g npm@^4
+# 
 sudo npm i -g gulp
-sudo npm i
+
+echo "Setup npm:"
+sudo npm i --save-dev
 gulp
 
 
-#php artisan migrate
-#php artisan db:seed
-php artisan key:generate
-composer dump-autoload --optimize
 
 
 wait
