cdedb89723dd1a5b91746e91a853b3001845cb1e
Metodi Deribanov <imaginois@gmail.com>
2017-05-01 22:11:53 +0300

    setup Treafik example

--- puphpet/files/startup-once/setup_traefik.sh
+++ puphpet/files/startup-once/setup_traefik.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd
-docker run -d -p 7070:7070 -p 70:70 -v $PWD/traefik.toml:/etc/traefik/traefik.toml traefik
+# docker run -d -p 7070:7070 -p 70:70 -v $PWD/traefik.toml:/etc/traefik/traefik.toml traefik
 
