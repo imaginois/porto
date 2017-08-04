839b9398db0a0cadf9dba906a10b97782879dd06
Metodi Deribanov <imaginois@gmail.com>
2017-08-04 14:19:32 +0300

    edit penates setup

--- puphpet/files/startup-once-unprivileged/penates-setup.sh
+++ puphpet/files/startup-once-unprivileged/penates-setup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cd /var/www/penates
-sudo npm i -g yarn webpack webpack-dev-server
+sudo apt-get upgrade nodejs -y
+sudo npm i -g yarn webpack@3 webpack-dev-server
 sudo yarn install
 yarn run build
