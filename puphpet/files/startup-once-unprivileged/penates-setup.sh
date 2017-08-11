a3cec0c28d896235559ca2085abbf0452e0989f1
Metodi Deribanov <imaginois@gmail.com>
2017-08-11 17:56:15 +0300

    penates setup building

--- puphpet/files/startup-once-unprivileged/penates-setup.sh
+++ puphpet/files/startup-once-unprivileged/penates-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd /var/www/penates
-sudo apt-get upgrade nodejs -y
+# sudo apt-get upgrade nodejs -y
 sudo npm i -g yarn webpack@3 webpack-dev-server
 sudo yarn install
 yarn run build
---
+++ puphpet/files/startup-once/setup_git_fusion.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+
+# sudo rm -rf /etc/apt/sources.list.d/repo.mysql*
+
+
+# cd /tmp
+# wget -qO - https://package.perforce.com/perforce.pubkey | sudo apt-key add -
+# touch /etc/apt/sources.list.d/perforce.list
+
+# echo "deb http://package.perforce.com/apt/ubuntu trusty release" | sudo tee --append /etc/apt/sources.list.d/perforce.list
+# apt-get update
+# apt-get install helix-git-fusion helix-git-fusion-base -y
+# apt-get autoremove -y
